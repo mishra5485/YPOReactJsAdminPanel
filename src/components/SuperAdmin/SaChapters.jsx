8dfab176c88d5283b8421ef37ad35c31a8e63235
Harsh Mishra <Harshmishra5485@gmail.com>
2024-11-28 16:54:51 +0530

    Added the Two Modals for Creation and Deletion of Chapter

--- src/components/SuperAdmin/SaChapters.jsx
+++ src/components/SuperAdmin/SaChapters.jsx
@@ -10,12 +10,19 @@ import { MdDeleteForever } from "react-icons/md";
 import { MdAdd } from "react-icons/md";
 
 const SaDashboardDetails = () => {
-  // const userData = useSelector((state) => state.auth.userData);
-
   const [records, setrecords] = useState([]);
   const [searchText, setSearchText] = useState("");
   const [filteredData, setFilteredData] = useState(records);
   const [isLoading, setisLoading] = useState(false);
+
+  const [isCreateChapterModalOpen, SetIsCreateChapterModalOpen] =
+    useState(false);
+  const openCreateChapterModal = () => SetIsCreateChapterModalOpen(true);
+  const closeCreateChapterModal = () => SetIsCreateChapterModalOpen(false);
+
+  const [isDeleteModalOpen, SetIsDeleteModalOen] = useState(false);
+  const openDeleteChapterModal = () => SetIsDeleteModalOen(true);
+  const closeDeleteChapterModal = () => SetIsDeleteModalOen(false);
 
   useEffect(() => {
     const filteredItems = records.filter((item) =>
@@ -195,13 +202,16 @@ const SaDashboardDetails = () => {
           </div>
         ) : (
           <>
-           
             <div className="bg-white w-full p-3 mx-auto rounded-md shadow-md max-w-7xl mt-5">
-            <div className="flex flex-row-reverse">
-              <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded my-4">
-              <MdAdd />
-              </button>
-            </div>
+              <div className="flex flex-row-reverse">
+                <button
+                  onClick={openCreateChapterModal}
+                  className="block text-white my-3 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                  type="button"
+                >
+                  <MdAdd />
+                </button>
+              </div>
               <div className="p-4 bg-gray-300 border-b border-gray-100 h-100vh">
                 <div
                   className="flex justify-around"
@@ -232,6 +242,144 @@ const SaDashboardDetails = () => {
                 />
               </div>
             </div>
+
+            {isCreateChapterModalOpen && (
+              <div
+                id="popup-modal"
+                className="overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full flex"
+              >
+                <div className="relative p-4 w-full max-w-md max-h-full">
+                  <div className="relative bg-white rounded-lg shadow dark:bg-gray-700">
+                    {/* Close button */}
+                    <button
+                      type="button"
+                      onClick={closeCreateChapterModal}
+                      className="absolute top-3 end-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white"
+                    >
+                      <svg
+                        className="w-3 h-3"
+                        aria-hidden="true"
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 14 14"
+                      >
+                        <path
+                          stroke="currentColor"
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          strokeWidth="2"
+                          d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"
+                        />
+                      </svg>
+                      <span className="sr-only">Close modal</span>
+                    </button>
+
+                    {/* Modal content */}
+                    <div className="p-4 md:p-5 text-center">
+                      <svg
+                        className="mx-auto mb-4 text-gray-400 w-12 h-12 dark:text-gray-200"
+                        aria-hidden="true"
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 20 20"
+                      >
+                        <path
+                          stroke="currentColor"
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          strokeWidth="2"
+                          d="M10 11V6m0 8h.01M19 10a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+                        />
+                      </svg>
+                      <h3 className="mb-5 text-lg font-normal text-gray-500 dark:text-gray-400">
+                        Are you sure you want to delete this product?
+                      </h3>
+                      <button
+                        onClick={closeCreateChapterModal}
+                        className="text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center"
+                      >
+                        Yes, I'm sure
+                      </button>
+                      <button
+                        onClick={closeCreateChapterModal}
+                        className="py-2.5 px-5 ms-3 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
+                      >
+                        No, cancel
+                      </button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            )}
+
+            {isDeleteModalOpen && (
+              <div
+                id="popup-modal"
+                className="overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full flex"
+              >
+                <div className="relative p-4 w-full max-w-md max-h-full">
+                  <div className="relative bg-white rounded-lg shadow dark:bg-gray-700">
+                    {/* Close button */}
+                    <button
+                      type="button"
+                      onClick={closeCreateChapterModal}
+                      className="absolute top-3 end-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white"
+                    >
+                      <svg
+                        className="w-3 h-3"
+                        aria-hidden="true"
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 14 14"
+                      >
+                        <path
+                          stroke="currentColor"
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          strokeWidth="2"
+                          d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"
+                        />
+                      </svg>
+                      <span className="sr-only">Close modal</span>
+                    </button>
+
+                    {/* Modal content */}
+                    <div className="p-4 md:p-5 text-center">
+                      <svg
+                        className="mx-auto mb-4 text-gray-400 w-12 h-12 dark:text-gray-200"
+                        aria-hidden="true"
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 20 20"
+                      >
+                        <path
+                          stroke="currentColor"
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          strokeWidth="2"
+                          d="M10 11V6m0 8h.01M19 10a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+                        />
+                      </svg>
+                      <h3 className="mb-5 text-lg font-normal text-gray-500 dark:text-gray-400">
+                        Are you sure you want to delete this product?
+                      </h3>
+                      <button
+                        onClick={closeCreateChapterModal}
+                        className="text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center"
+                      >
+                        Yes, I'm sure
+                      </button>
+                      <button
+                        onClick={closeCreateChapterModal}
+                        className="py-2.5 px-5 ms-3 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
+                      >
+                        No, cancel
+                      </button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            )}
           </>
         )}
       </div>
