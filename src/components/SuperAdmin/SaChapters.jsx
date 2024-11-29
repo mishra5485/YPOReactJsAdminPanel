9f51d667c7c36c767937f1305d3ba5165f2e29fd
Harsh Mishra <Harshmishra5485@gmail.com>
2024-11-29 13:59:44 +0530

    removed unused and commented codes

--- src/components/SuperAdmin/SaChapters.jsx
+++ src/components/SuperAdmin/SaChapters.jsx
@@ -210,15 +210,6 @@ const SaDashboardDetails = () => {
         ) : (
           <>
             <div className="bg-white w-full p-3 mx-auto rounded-md shadow-md">
-              {/* <div className="flex flex-row-reverse">
-                <button
-                  onClick={() => SetIsCreateChapterModalOpen(true)}
-                  className="block text-white my-3 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-                  type="button"
-                >
-                  <MdAdd size={24} />
-                </button>
-              </div> */}
               <div className="mt-10 border-b border-gray-100 h-100vh">
                 <div
                   className="flex justify-around my-5"
