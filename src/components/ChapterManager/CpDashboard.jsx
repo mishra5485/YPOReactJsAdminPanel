2e47c9cb19c2717d05eca31fa557f39fb3b9037e
Harsh Mishra <Harshmishra5485@gmail.com>
2024-12-03 14:22:47 +0530

    updated the Chapter manager Dashboard Component

--- src/components/ChapterManager/CpDashboard.jsx
+++ src/components/ChapterManager/CpDashboard.jsx
@@ -1,19 +1,37 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 import Sidebar from "./CpSidebar";
+import { FaUser } from "react-icons/fa";
+import { Link } from "react-router-dom";
+import getLoginData from "../../commonfunctions/getLoginData";
 
-const SuperAdminDashBoard = () => {
+const CpDashboard = () => {
+  const LoginData = getLoginData();
+
   return (
-    <div className="h-[100%] ">
-      {/* Sidebar remains fixed on the left */}
+    <div className="h-[100%]">
       <Sidebar />
 
-      {/* Right side displays the nested routes */}
-      <div className="p-4 sm:ml-72 bg-primaryBg">
+      <div className="p-4 sm:ml-64 h-[100vh]">
+        <div className="w-full flex justify-between items-center mt-4 h-16 px-4 pt-4 pb-12 border-b-2">
+          <div>
+            <h1 className="text-xl sm:text-2xl lg:text-3xl font-bold">
+              {`Welcome Back, ${LoginData.Username}`}
+            </h1>
+          </div>
+          <Link to="/sp/dashboard/saprofile">
+            <div className="p-3 sm:p-4 border rounded-full shadow-md">
+              <FaUser
+                size={20} 
+                className="sm:size-18 lg:size-6" 
+              />
+            </div>
+          </Link>
+        </div>
         <Outlet />
       </div>
     </div>
   );
 };
 
-export default SuperAdminDashBoard;
+export default CpDashboard;
