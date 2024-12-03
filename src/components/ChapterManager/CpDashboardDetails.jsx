d8e3e30a02dde605fe13daee17cb27c6e44cc897
Harsh Mishra <Harshmishra5485@gmail.com>
2024-12-03 16:03:12 +0530

    Updated the dashboard chapterManagers Data

--- src/components/ChapterManager/CpDashboardDetails.jsx
+++ src/components/ChapterManager/CpDashboardDetails.jsx
@@ -1,16 +1,13 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import toast, { Toaster } from "react-hot-toast";
-import { Link } from "react-router-dom";
-import { FaUsers } from "react-icons/fa";
-import { IoBagAdd } from "react-icons/io5";
+import { FaUsers, FaRegClock } from "react-icons/fa";
 import getLoginData from "../../commonfunctions/getLoginData";
 
 const CpDashboardDetails = () => {
   const LoginData = getLoginData();
 
-
-  const [dashboardData, setDashboardData] = useState({});
+  const [dashboardData, setDashboardData] = useState([]);
   const [isLoading, setisLoading] = useState(false);
 
   useEffect(() => {
@@ -26,7 +23,7 @@ const CpDashboardDetails = () => {
           import.meta.env.VITE_REACT_APP_BASE_URL
         }/user/getChapterManagerDashBoardData`,
         {
-          chapter_manager_id:LoginData.user_id
+          chapter_manager_id: LoginData.user_id,
         }
       );
 
@@ -64,7 +61,6 @@ const CpDashboardDetails = () => {
   return (
     <>
       <Toaster />
-      {console.log("isLoading=====>", isLoading)}
       <div>
         {isLoading ? (
           <div className="flex justify-center">
@@ -89,91 +85,57 @@ const CpDashboardDetails = () => {
             </div>
           </div>
         ) : (
-          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 p-4 mt-10">
-            {/* Regional Management Card */}
-            <Link to="/sp/dashboard/saadminstable">
-              <div className="bg-gray-50 shadow-lg rounded-lg p-6 flex flex-col items-start text-center">
-                <div className="mb-4 bg-amber-500 p-3 border border-gray-200 rounded-xl shadow-md">
-                  <FaUsers size={32} color="white" />
-                </div>
-                <div className="text-lg font-semibold mb-2">
-                  Regional Management
-                </div>
-                <div className="text-2xl font-bold">
-                  {dashboardData.superAdminsDatalength}
-                </div>
-              </div>
-            </Link>
+          dashboardData?.map((data, index) => {
+            return (
+              <>
+                <div key={index} className="my-6 border-t-2 first:border-none p-6">
+                  <p className="font-semibold text-2xl text-gray-800">
+                    {data.chapterName}
+                  </p>
+                  <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-5 gap-6 p-4 mt-5 ">
+                    {/* Members Card */}
+                    <div className="bg-white shadow-lg rounded-lg p-6 flex flex-col items-center text-center transform transition-transform hover:scale-105">
+                      <div className="mb-4 bg-amber-500 p-3 border border-gray-200 rounded-full shadow-md">
+                        <FaUsers size={32} color="white" />
+                      </div>
+                      <div className="text-lg font-semibold text-gray-700 mb-2 h-10">
+                        Members
+                      </div>
+                      <div className="text-3xl font-extrabold text-gray-900">
+                        {data.membersDataCount}
+                      </div>
+                    </div>
 
-            {/* Members Card */}
-            <Link to="/sp/dashboard/samembers">
-              <div className="bg-gray-50 shadow-lg rounded-lg p-6 flex flex-col items-start text-center">
-                <div className="mb-4 bg-amber-500 p-3 border border-gray-200 rounded-xl shadow-md">
-                  <FaUsers size={32} color="white" />
-                </div>
-                <div className="text-lg font-semibold mb-2">Members</div>
-                <div className="text-2xl font-bold">
-                  {dashboardData.membersDatalength}
-                </div>
-              </div>
-            </Link>
+                    {/* Spouse/Partners Card */}
+                    <div className="bg-white shadow-lg rounded-lg p-6 flex flex-col items-center text-center transform transition-transform hover:scale-105">
+                      <div className="mb-4 bg-amber-500 p-3 border border-gray-200 rounded-full shadow-md">
+                        <FaUsers size={32} color="white" />
+                      </div>
+                      <div className="text-lg font-semibold text-gray-700 mb-2">
+                        Spouse/Partners
+                      </div>
+                      <div className="text-3xl font-extrabold text-gray-900">
+                        {data.spousePartnersDataCount}
+                      </div>
+                    </div>
 
-            {/* Spouse/Partners Card */}
-            <Link to="/sp/dashboard/saspousepartners">
-              <div className="bg-gray-50 shadow-lg rounded-lg p-6 flex flex-col items-start text-center">
-                <div className="mb-4 bg-amber-500 p-3 border border-gray-200 rounded-xl shadow-md">
-                  <FaUsers size={32} color="white" />
+                    {/* Pending Approvals Card */}
+                    <div className="bg-white shadow-lg rounded-lg p-6 flex flex-col items-center text-center transform transition-transform hover:scale-105">
+                      <div className="mb-4 bg-amber-500 p-3 border border-gray-200 rounded-full shadow-md">
+                        <FaRegClock size={32} color="white" />
+                      </div>
+                      <div className="text-lg font-semibold text-gray-700 mb-2">
+                        Pending Approvals
+                      </div>
+                      <div className="text-3xl font-extrabold text-gray-900">
+                        {data.pendingApprovalsDataCount}
+                      </div>
+                    </div>
+                  </div>
                 </div>
-                <div className="text-lg font-semibold mb-2">
-                  Spouse/Partners
-                </div>
-                <div className="text-2xl font-bold">
-                  {dashboardData.spousePartnersDatalength}
-                </div>
-              </div>
-            </Link>
-
-            {/* Chapters Card */}
-            <Link to="/sp/dashboard/sachapters">
-              <div className="bg-gray-50 shadow-lg rounded-lg p-6 flex flex-col items-start text-center">
-                <div className="mb-4 bg-amber-500 p-3 border border-gray-200 rounded-xl shadow-md">
-                  <FaUsers size={32} color="white" />
-                </div>
-                <div className="text-lg font-semibold mb-2">Chapters</div>
-                <div className="text-2xl font-bold">
-                  {dashboardData.allChaptersDatalength}
-                </div>
-              </div>
-            </Link>
-
-            {/* Chapter Managers Card */}
-            <Link to="/sp/dashboard/sachaptermanagers">
-              <div className="bg-gray-50 shadow-lg rounded-lg p-6 flex flex-col items-start text-center">
-                <div className="mb-4 bg-blue-500 p-3 border border-gray-200 rounded-xl shadow-md">
-                  <IoBagAdd size={32} color="white" />
-                </div>
-                <div className="text-lg font-semibold mb-2">
-                  Chapter Managers
-                </div>
-                <div className="text-2xl font-bold">
-                  {dashboardData.chapterManagersDatalength}
-                </div>
-              </div>
-            </Link>
-
-            {/* Pending Approval Card */}
-            <Link to="/sp/dashboard/sarequests">
-              <div className="bg-gray-50 shadow-lg rounded-lg p-6 flex flex-col items-start text-center">
-                <div className="mb-4 bg-amber-500 p-3 border border-gray-200 rounded-xl shadow-md">
-                  <FaUsers size={32} color="white" />
-                </div>
-                <div className="text-lg font-semibold mb-2">
-                  Pending Approval
-                </div>
-                <div className="text-2xl font-bold">2</div>
-              </div>
-            </Link>
-          </div>
+              </>
+            );
+          })
         )}
       </div>
     </>
