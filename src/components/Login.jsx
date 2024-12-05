a505118e7ecd64c3568a54b4bd734432e9e3e0fd
Harsh Mishra <Harshmishra5485@gmail.com>
2024-12-05 12:34:33 +0530

    Added the login Page Logo and updated the Login Component

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import logo from "../assets/logo.jpeg";
+import logo from "../assets/logo.png";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
 import toast, { Toaster } from "react-hot-toast";
