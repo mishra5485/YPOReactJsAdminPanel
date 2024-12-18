508817658f624ecd14895f02df3ccd1946d5b372
Harsh Mishra <Harshmishra5485@gmail.com>
2024-12-18 10:30:52 +0530

    updated the vite config file

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,16 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
+
 export default defineConfig({
   plugins: [react()],
+  base: "/",
   server: {
     host: "0.0.0.0",
     port: 5173,
+  },
+  build: {
+    outDir: "dist",
+    emptyOutDir: true,
   },
 });
